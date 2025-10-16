6da7d445561e08f8fc499251280bb9c27113be6e
nikhilpantha <nikhilpantha207@gmail.com>
2025-10-16 13:00:29 +0545

    update image url

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.inseed.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "inseed.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
